ebaaff25ca2f05b576944f7ccdfe5e17cdec57d8
BARDAN1172 <leob97878@gmail.com>
2026-02-11 03:09:54 +0800

    Update SpotifyPlayer.tsx

--- app/SpotifyPlayer.tsx
+++ app/SpotifyPlayer.tsx
@@ -4,9 +4,10 @@ export default function SpotifyPlayer() {
   return (
     <div className="w-full rounded-[12px] overflow-hidden shadow-2xl border border-zinc-800 bg-black">
       <iframe 
+        data-testid="embed-iframe"
         title="Spotify Playlist"
         style={{ borderRadius: "12px" }} 
-        src="https://open.spotify.com/embed/playlist/37i9dQZF1E37j9dQZF1E3?utm_source=generator&theme=0" 
+        src="https://open.spotify.com/embed/pl" 
         width="100%" 
         height="352" 
         frameBorder="0" 
